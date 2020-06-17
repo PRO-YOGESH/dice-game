a21d1f8ebc25905855ad73699888f156c2741b77
PRO-YOGESH <65109119+PRO-YOGESH@users.noreply.github.com>
2020-06-17 12:10:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+
+var randomNumber1 ,randomNumber2;
+
+
+function myFunction1(){
+  randomNumber1 = Math.floor(Math.random() * 6) + 1 ;
+  var randomImage1 = "images/dice" + randomNumber1 + ".png" ;
+   document.querySelectorAll("img")[0].setAttribute("src",randomImage1);
+   document.querySelector("h1").innerHTML = "GO PLAYER 2" ;
+  document.getElementById("b-2").style.visibility = "visible" ;
+    document.getElementById("b-1").style.visibility = "hidden" ;
+}
+function myFunction2(){
+ randomNumber2 = Math.floor(Math.random() * 6) + 1 ;
+  var randomImage2 = "images/dice" + randomNumber2 + ".png" ;
+   document.querySelectorAll("img")[1].setAttribute("src",randomImage2);
+    document.getElementById("b-2").style.visibility = "hidden" ;
+
+ if(randomNumber1 > randomNumber2)
+ {
+   document.querySelector("h1").innerHTML = "✔ Player1 WON";
+
+ }
+
+ else if (randomNumber1< randomNumber2) {
+   document.querySelector("h1").innerHTML = "✔ Player2 WON";
+ }
+
+ else{
+   document.querySelector("h1").innerHTML = "🤔 ITS A DRAW ";
+ }
+
+ document.getElementById("play").style.visibility = "visible" ;
+
+}
+
+function myFunction3(){
+  document.getElementById("play").style.visibility = "hidden" ;
+  document.getElementById("b-1").style.visibility = "visible" ;
+   document.querySelector("h1").innerHTML = "GO PLAYER 1" ;
+}
